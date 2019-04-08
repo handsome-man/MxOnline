e32543788087aa4d64664d4b53015af602840fe2
GESION\LuNan.Liu <lunan.liu@gesion.net>
2019-04-08 18:02:32 +0800

    授课教师页面逻辑

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -3,15 +3,39 @@ from django.shortcuts import render
 from django.views.generic import View
 # Create your views here.
 
-from organization.models import CityDict, CourseOrg
+from organization.models import CityDict, CourseOrg, Teacher
 
 
 class TeacherListView(View):
     """教师列表"""
-
     @staticmethod
     def get(request):
-        return render(request, 'teachers-list.html')
+        # 所有的老师
+        all_teacher = Teacher.objects.all()
+
+        # 搜索框搜索内容
+        keywords = request.GET.get('keywords', '')
+        if keywords:
+            # i忽略大小写
+            all_teacher = all_teacher.filter(name__icontains=keywords)
+
+        # 讲师排行榜
+        hot_teacher = all_teacher.order_by('-work_years')[:2]
+
+        # 人气
+        sort = request.GET.get('sort', '')
+        if sort:
+            all_teacher = all_teacher.order_by('-click_nums')
+
+        # 共多少人
+        teacher_count = all_teacher.count()
+
+        return render(request, 'teachers-list.html', {
+            'all_teacher': all_teacher,
+            'sort': sort,
+            'hot_teacher': hot_teacher,
+            'teacher_count': teacher_count,
+        })
 
 
 class OrganizationListView(View):
@@ -38,7 +62,7 @@ class OrganizationListView(View):
             all_course_org = all_course_org.filter(city=int(city_id))
 
         # 授课机构排名
-        hot_orgs = all_course_org.order_by('-click_nums')[:3]
+        hot_orgs = all_course_org.order_by('-click_nums')[:5]
 
         # 热度
         sort = request.GET.get('sort', '')
